4655a63b5c671419526e8ed2de903c75926602fa
STU-Idichi-Syoya <55622475+STU-Idichi-Syoya@users.noreply.github.com>
2020-09-12 00:07:06 +0900

    Update sensor_temp.py

--- sensor_temp.py
+++ sensor_temp.py
@@ -25,18 +25,19 @@ def get_tmp():
     stime=time.time()
     try:
         i2c.write_i2c_block_data(address,0x00,[])
-    except:
-        pass
-    # 読み取り命令
-    time.sleep(0.003)
-    i2c.write_i2c_block_data(address,0x03,[0x00,0x04])
-     
-    # データ受取
-    time.sleep(0.015)
-    block = i2c.read_i2c_block_data(address,0,6)
-    hum = float(block[2] << 8 | block[3])/10
-    tmp = float(block[4] << 8 | block[5])/10
     
+      # 読み取り命令
+      time.sleep(0.003)
+      i2c.write_i2c_block_data(address,0x03,[0x00,0x04])
+
+      # データ受取
+      time.sleep(0.015)
+      block = i2c.read_i2c_block_data(address,0,6)
+      hum = float(block[2] << 8 | block[3])/10
+      tmp = float(block[4] << 8 | block[5])/10
+    except :
+     print("get_tmp()::ERR!")
+     return btemp
     btemp=tmp
     return tmp
 
